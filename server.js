93b34ab1af3966d75f52d627635a27d9ff7462bf
ziad40 <zizoabuelkher@gmail.com>
2025-10-28 01:24:56 +0300

    fix regex error

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use('/api/users', usersRoute);
 app.use('/api/auth', authRoute);
 app.use('/api/problem', problemRoute)
 app.use(express.static(path.join(__dirname, 'client', 'build')));
-app.get('*', (req, res) =>
+app.get(/.*/, (req, res) =>
   res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
 );
 
