96bb2f0a5a837e2bceb32147aa964725163aac37
ziad40 <zizoabuelkher@gmail.com>
2025-10-30 13:53:14 +0300

    almost set prompt

--- controllers/botController.js
+++ controllers/botController.js
@@ -6,7 +6,12 @@ exports.showHint = async (req, res) => {
     if (!type || !answer || !solution) {
         return res.status(400).json({ error: 'Question type is required, answer is required, solution is required' });
     }
-
+    // clean student answer before submit
+    let operations = answer.operations
+    Object.entries(operations).forEach(([key, value]) => {
+        // Remove all spaces from the value
+        operations[key] = value.replace(/\s+/g, '');
+    });
     try {
         const completion = await openaiClient.chat.completions.create({
             model: 'openai/gpt-4o',
@@ -17,8 +22,8 @@ exports.showHint = async (req, res) => {
                                 Their answer is ${JSON.stringify(answer)}, 
                                 the correct solution is ${JSON.stringify(solution)}. 
                                 Please provide one and only one very short, simple hint — not the full answer.
-                                and if they works well, then give general hint about Scheduling algorithm.
-                                I want respone to be forward directly to them
+                                and if answer and correct solution matches, then encourage them and give general hint about this Scheduling algorithm.
+                                I want respone to be declared as it will be forward directly to them without any updates I will do
                                 `
 
                 },
